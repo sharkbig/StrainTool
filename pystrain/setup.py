7be686c92cef710137e4b5073f57c28777b55e0a
xanthos <xanthos.papanikolaou@gmail.com>
2018-07-08 20:02:04 +0300

    fixed versioning

--- pystrain/setup.py
+++ pystrain/setup.py
@@ -1,11 +1,28 @@
 import setuptools
+import codecs
+import os
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("[ERROR] Unable to find version string.")
 
 setuptools.setup(name='pystrain',
-      version='1.0',
+      version=find_version("pystrain", "__init__.py"),
       description='Python Strain Tensor estimation tool.',
       url='https://github.com/DSOlab/StrainTool.git',
       author='Xanthos Papanikolaou, Demitris Anastasiou',
       author_email='xanthos@mail.ntua.gr, danast@mail.ntua.gr',
-      packages=setuptools.find_packages(),#['pystrain', 'pystrain.geodesy', 'pystrain.iotools'],
+      packages=setuptools.find_packages(),
       install_requires=['numpy', 'scipy']
       )
